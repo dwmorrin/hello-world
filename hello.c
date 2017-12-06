484929f7157b7b61c607f07f75e6d8b31e74dd6a
dwmorrin <dwmorrin@gmail.com>
2017-12-06 11:02:11 -0500

    updating hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello\n");
+    size_t bufsize = 32;
+    char *buffer = malloc(bufsize * sizeof *buffer);
+    int index = 0;
+    if (buffer == NULL) {
+        puts("Error allocating memory");
+        abort();
+    }
+    
+    do {
+        if (buffer[0] == '\n')
+            printf("You don't have a name? Try again.\n");
+        printf("What is your name? ");
+    } while(!getline(&buffer, &bufsize, stdin) || buffer[0] == '\n');
+    
+    while(buffer[++index] != '\n');
+    buffer[index] = '\0';
+    printf("Hello, %s\n", buffer);
     
     return 0;
 }
