40af8c43c4178b6b0e8f438e6c5014e99bb70317
dwmorrin <dwmorrin@gmail.com>
2017-12-13 19:35:53 -0500

    updating hello.c to include EOF handling

--- hello.c
+++ hello.c
@@ -4,24 +4,39 @@ hello.c asks for the user's name and then echoes the name with "Hello, <name>"
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    size_t bufsize = 32;
-    char *buffer = malloc(bufsize * sizeof *buffer);
+int main()
+{
     int index = 0;
+    size_t bufsize = 32;
+    char *buffer = malloc(bufsize * sizeof *buffer);    
+    ssize_t nbytes = 0;
+    /* Check that buffer is OK */
     if (buffer == NULL) {
         puts("Error allocating memory");
         abort();
     }
     
+    /* Get user's name  */
     do {
+        if (nbytes == 1) { /* implies EOF was entered */
+            puts("");
+            puts("EOF? Use ctrl-c to abort.");
+        }
         if (buffer[0] == '\n')
-            printf("You don't have a name? Try again.\n");
+            puts("You don't have a name? Try again.");
         printf("What is your name? ");
-    } while(!getline(&buffer, &bufsize, stdin) || buffer[0] == '\n');
+    } while((nbytes = getline(&buffer, &bufsize, stdin) == -1)
+        || buffer[0] == '\n');
     
+    /* Remove newline from buffer */
     while(buffer[++index] != '\n');
     buffer[index] = '\0';
+    
+    /* Display message to the user */
     printf("Hello, %s\n", buffer);
+    
+    /* Clean up */
+    free(buffer);
     
     return 0;
 }
